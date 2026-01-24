631f2bb3a90d3eaeabfba8aef21a19d1af47bfad
Prithvi Paturi <prithvi.paturi@gmail.com>
2026-01-24 14:18:57 -0500

    added new functionality to search

---  model/preprocess.py
+++  model/preprocess.py
@@ -2,6 +2,4 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, normalize
 
 def load_and_preprocess(csv_path='../data/Comparison Stats.csv'):
-
     df = pd.read_csv(csv_path)
-
